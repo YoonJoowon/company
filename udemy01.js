21625c65dc415d0f1f64ddaa9aaa2d4246a344e7
joowon yoon <wndnjs6224@naver.com>
2023-02-10 16:59:38 +0900

    0210

--- udemy01.js
+++ udemy01.js
@@ -42,3 +42,58 @@
         return str2.length - srt1.length;
     }
     ["Steele", "Colt", "Data Structures", "Algorithms"].sort(compareByLen)
+
+    // 버블 정렬 : 구현
+
+    function bubblesort(arr){
+        for(var i = arr.length; i > 0; i--){
+            for(var j = 0; j< i-1; j++){
+                console.log(arr, arr[j], arr[j+1]);
+                if(arr[j] > arr[j+1]){
+                    // SWAP!
+                    var temp = arr[j];
+                    arr[j] = arr[j+1];
+                    arr[j+1] = temp;                    
+                }
+            }
+            console.log("ONE PASS COMPLETE!")
+        }
+        return arr;
+    }
+
+    function bubbleSort(arr) {
+        let noSwap;
+        for (var i = arr.length; i > 0; i--) {
+          noSwap = true;
+          for (var j = 0; j < i - 1; j++) {
+            if (arr[j] > arr[j + 1]) {
+              var temp = arr[j];
+              arr[j] = arr[j + 1];
+              arr[j + 1] = temp;
+              noSwap = false;
+            }
+          }
+          if (noSwap) break;
+        }
+        return arr;
+      }
+      
+    bubblesort([37,45,29,8,12]);
+
+    
+
+    function bubblesort(arr){
+        const swap = (arr, idx1, idx2) => {
+            [arr[idx1], arr[idx2]] = [arr[idx2], arr[idx1]];
+        };
+
+        for (let i = arr.length; i>0; i--){
+            for(let j = 0; j < i-1; j++){
+                if(arr[j] > arr[j+1]){
+                    swap(arr, j, j+1);
+                }
+            }
+        }
+        return arr;
+    }
+    bubblesort([37,45,29,8,12]);
