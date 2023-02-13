5c841a5ba81899127701788424e709b314fd2cc0
joowon yoon <wndnjs6224@naver.com>
2023-02-13 15:35:33 +0900

    0213

--- udemy05.js
+++ udemy05.js
@@ -1,99 +1,41 @@
-// 검색 알고리즘
-
-    // 이진 검색 솔루션
-
-    function Search(arr, elem){
-        var start = 0;
-        var end = arr.length -1;
-        var middle = math.floor((start + end) / 2);
-        while(arr[middle] !== elem && start <= end){
-            if(elem < arr[middle]) end = middle - 1;
-            else start = middle + 1;
-            middle = Math.floor((start + end) / 2);
-        }
-        return arr[middle] === elem ? middle : -1;
-    }
-    Search([2,5,6,7,13,15,28,30], -10)
-
-    // 나이브 문자열 검색구현
-
-    function naiveSearch(long, short){
-        var count = 0;
-        for (var i = 0; i < long.length; i++){
-            for(var j = 0; j < short.length; j++){
-                if(short[j] !== long[i+j]) break;
-                if(j === short.length - 1) count ++;
-            }
-        }
-        return count;
-    }
-    naiveSearch("lorie loled", "pop")
-
 // 버블 정렬
 
-    // 기본 내장 자바스크립트 정렬
+    // 선택정렬
 
-    function numberCompare(num1, num2){
-        return num2 - num1;
-    }
-    [6, 4, 15, 10].sort(numberCompare);
-
-    function compareByLen(srt1, str2){
-        return str2.length - srt1.length;
-    }
-    ["Steele", "Colt", "Data Structures", "Algorithms"].sort(compareByLen)
-
-    // 버블 정렬 : 구현
-
-    function bubblesort(arr){
-        for(var i = arr.length; i > 0; i--){
-            for(var j = 0; j< i-1; j++){
-                console.log(arr, arr[j], arr[j+1]);
-                if(arr[j] > arr[j+1]){
-                    // SWAP!
-                    var temp = arr[j];
-                    arr[j] = arr[j+1];
-                    arr[j+1] = temp;                    
+    function selectionSort(arr){
+        for(var i = 0; i < arr.length; i++){
+            var lowest = i;
+            for(var j = i+1; j < arr.length; j++){
+                if(arr[j] < arr[lowest]){
+                    lowest = j;
                 }
             }
-            console.log("ONE PASS COMPLETE!")
+            if(i !== lowest){
+                var temp = arr[i];
+                arr[i] = arr[lowest];
+                arr[lowest] = temp;
+            }
         }
         return arr;
     }
-
-    function bubbleSort(arr) {
-        let noSwap;
-        for (var i = arr.length; i > 0; i--) {
-          noSwap = true;
-          for (var j = 0; j < i - 1; j++) {
-            if (arr[j] > arr[j + 1]) {
-              var temp = arr[j];
-              arr[j] = arr[j + 1];
-              arr[j + 1] = temp;
-              noSwap = false;
-            }
-          }
-          if (noSwap) break;
-        }
-        return arr;
-      }
-      
-    bubblesort([37,45,29,8,12]);
+    selectionSort([34,22,10,19,17]);
 
     
+    function selectionSort(arr){
+        const swap = (arr, idx1, idx2) =>
+        ([arr[idx1], arr[idx2]] = [arr[idx2], arr[idx1]])
 
-    function bubblesort(arr){
-        const swap = (arr, idx1, idx2) => {
-            [arr[idx1], arr[idx2]] = [arr[idx2], arr[idx1]];
-        };
-
-        for (let i = arr.length; i>0; i--){
-            for(let j = 0; j < i-1; j++){
-                if(arr[j] > arr[j+1]){
-                    swap(arr, j, j+1);
+        for(let i = 0; i < arr.length; i++){
+            let lowest = i;
+            for (let j = i+1; j < arr.length; j++){
+                if(arr[lowest] > arr[j]){
+                    lowest = j;
                 }
             }
+            if(i !== lowest) swap(arr, i, lowest);
         }
         return arr;
     }
-    bubblesort([37,45,29,8,12]);
+
+    selectionSort([0,2,34,22,10,19,17])
+    
