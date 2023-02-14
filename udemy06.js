66ded84e13bc1498b992fb2964749520f47acad3
joowon yoon <wndnjs6224@naver.com>
2023-02-14 17:33:58 +0900

    0214

--- udemy06.js
+++ udemy06.js
@@ -1,2 +1,15 @@
 // 삽입정렬
     
+    // 구현
+    function insertionSort(arr){
+        for(var i = 1; i < arr.length; i++){
+            var currentVal = arr[i];
+            for(var j = i-1; j >= 0 && arr[j] > currentVal; j--){
+                arr[j+1] = arr[j]
+            }
+            arr[j+1] = currentVal;
+            console.log(arr)
+        }
+        return arr;
+    }
+    insertionSort([2,1,9,76,3])
